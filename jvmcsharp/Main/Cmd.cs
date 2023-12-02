34d482045abdc8c49dc4e9577614177fbcc6ae94
blessingIX <1350437095@qq.com>
2023-12-02 17:53:22 +0800

    命令行工具

---
+++ jvmcsharp/Main/Cmd.cs
@@ -0,0 +1,57 @@
+﻿namespace jvmcsharp.Main
+{
+    internal struct Cmd
+    {
+        public bool HelpFlag { get; private set; }
+        public bool VersionFlag { get; private set; }
+        public string CpOption { get; private set; }
+        public string Class { get; private set; }
+        public string[] Args { get; private set; }
+
+        internal static Cmd ParseCmd()
+        {
+            var cmd = new Cmd();
+            var args = Environment.GetCommandLineArgs().Skip(1).ToList();
+            Var(args, v => cmd.HelpFlag = v, ["help", "?"], false);
+            Var(args, v => cmd.VersionFlag = v, ["version"], false);
+            Var(args, v => cmd.CpOption = v, ["classpath", "cp"], string.Empty);
+            if (args.Count > 0)
+            {
+                cmd.Class = args[0];
+                cmd.Args = args.Skip(1).ToArray();
+            }
+            return cmd;
+        }
+
+        internal void PrintUsage()
+        {
+            Console.WriteLine($"Usage: {Environment.GetCommandLineArgs()[0]} [-options] class [args...]");
+        }
+
+        internal static void Var(List<string> args, Action<bool> action, string[] names, bool defalut)
+        {
+            foreach (var name in names)
+            {
+                var found = args.Remove($"-{name}");
+                if (!found) continue;
+
+                action(found || defalut);
+                action = v => { };
+            }
+        }
+
+        internal static void Var(List<string> args, Action<string> action, string[] names, string defalut)
+        {
+            foreach (var name in names)
+            {
+                var idx = args.IndexOf($"-{name}");
+                if (idx < 0) continue;
+                var value = args.Count > idx + 1 ? args[idx + 1] : string.Empty;
+                action(value ?? defalut);
+                action = v => { };
+                if (idx < args.Count) args.RemoveAt(idx);
+                if (idx < args.Count) args.RemoveAt(idx);
+            }
+        }
+    }
+}
--- jvmcsharp/Program.cs
+++ jvmcsharp/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace jvmcsharp
+﻿using jvmcsharp.Main;
+
+namespace jvmcsharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var cmd = Cmd.ParseCmd();
+            if (cmd.VersionFlag)
+            {
+                Console.WriteLine("version 0.0.1");
+            }
+            else if (cmd.HelpFlag || string.IsNullOrEmpty(cmd.Class))
+            {
+                cmd.PrintUsage();
+            }
+            else
+            {
+                StartJVM(cmd);
+            }
+        }
+
+        static void StartJVM(Cmd cmd)
+        {
+            Console.WriteLine($"classpath:{cmd.CpOption} class:{cmd.Class} args:[{string.Join(" ", cmd.Args)}]");
         }
     }
 }
