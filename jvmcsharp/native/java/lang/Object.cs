5c8e5f964d07d4e5bbdd072efcb410c5ff059f1e
blessingIX <1350437095@qq.com>
2023-12-15 16:33:33 +0800

    Object.hashCode()

--- jvmcsharp/native/java/lang/Object.cs
+++ jvmcsharp/native/java/lang/Object.cs
@@ -7,6 +7,7 @@ namespace jvmcsharp.native.java.lang
         static Object()
         {
             Registry.Register("java/lang/Object", "getClass", "()Ljava/lang/Class;", GetClass);
+            Registry.Register("java/lang/Object", "hashCode", "()I", HashCode);
         }
 
         private static void GetClass(Frame frame)
@@ -14,6 +15,13 @@ namespace jvmcsharp.native.java.lang
             var @this = frame.LocalVars.GetThis();
             var @class = @this.Class.JClass;
             frame.OperandStack.Push(@class);
+        }
+
+        private static void HashCode(Frame frame)
+        {
+            var @this = frame.LocalVars.GetThis();
+            var hash = @this.GetHashCode();
+            frame.OperandStack.Push(hash);
         }
     }
 }
